e9c2371c915b89eedcb1cbd34a388e56ac955617
laryssarodrigues <laryssa.rodrigues@ufrpe.br>
2026-01-13 08:16:44 -0300

    elimination of redundant conditions + input handling

--- src/Classifier.java
+++ src/Classifier.java
@@ -3,43 +3,55 @@ import java.util.Scanner;
 public class Classifier {
     public static void main(String[] args) {
 
-        String name;
-        String level = "";
+        Scanner sc = new Scanner(System.in);
+
+        String name, level;
         int xp;
 
-        Scanner sc = new Scanner(System.in);
+        System.out.println(">>>|HERO LEVEL CLASSIFICATION SYSTEM|<<<");
 
         System.out.print("Hero's name: ");
         name = sc.nextLine();
 
         System.out.print("Amount of experience (XP): ");
-        xp = sc.nextInt();
+        while (true) {
+            if (sc.hasNextInt()) {
+                xp = sc.nextInt();
+                break;
+            } else {
+                System.out.println("Invalid entry. Please enter only numbers!");
+                System.out.print("Try again: ");
+                sc.next();
+            }
+        }
 
         if (xp < 1000) {
             level = "Iron";
 
-        } else if (xp >= 1001 && xp <= 2000) {
+        } else if (xp <= 2000) {
             level = "Bronze";
 
-        } else if (xp >= 2001 && xp <= 5000) {
+        } else if (xp <= 5000) {
             level = "Silver";
 
-        } else if (xp >= 5001 && xp <= 7000) {
+        } else if (xp <= 7000) {
             level = "Gold";
 
-        } else if (xp >= 7001 && xp <= 8000) {
+        } else if (xp <= 8000) {
             level = "Platinum";
 
-        } else if (xp >= 8001 && xp <= 9000) {
+        } else if (xp <= 9000) {
             level = "Ascending";
 
-        } else if (xp >= 9001 && xp <= 10000) {
+        } else if (xp <= 10000) {
             level = "Immortal";
 
-        } else if (xp >= 10001) {
+        } else {
             level = "Radiant";
         }
 
-        System.out.printf("The hero named %s is at the %s level", name, level);
+        System.out.printf(">>The hero named %s is at the %s level", name, level);
+
+        sc.close();
     }
 }
